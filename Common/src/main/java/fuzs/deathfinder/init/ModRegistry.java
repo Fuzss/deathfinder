2c1f98ae7451820eb163e76dd845de2756f35b81
Fuzss <28218241+Fuzss@users.noreply.github.com>
2023-05-27 21:46:37 +0200

    1.19.4 port

--- Common/src/main/java/fuzs/deathfinder/init/ModRegistry.java
+++ Common/src/main/java/fuzs/deathfinder/init/ModRegistry.java
@@ -5,11 +5,11 @@ import fuzs.deathfinder.capability.PlayerDeathTracker;
 import fuzs.deathfinder.capability.PlayerDeathTrackerImpl;
 import fuzs.puzzleslib.api.capability.v2.CapabilityController;
 import fuzs.puzzleslib.api.capability.v2.data.CapabilityKey;
-import fuzs.puzzleslib.api.capability.v2.data.PlayerRespawnStrategy;
+import fuzs.puzzleslib.api.capability.v2.data.PlayerRespawnCopyStrategy;
 
 public class ModRegistry {
     private static final CapabilityController CAPABILITIES = CapabilityController.from(DeathFinder.MOD_ID);
-    public static final CapabilityKey<PlayerDeathTracker> PLAYER_DEATH_TRACKER_CAPABILITY = CAPABILITIES.registerPlayerCapability("death_tracker", PlayerDeathTracker.class, player -> new PlayerDeathTrackerImpl(), PlayerRespawnStrategy.ALWAYS_COPY);
+    public static final CapabilityKey<PlayerDeathTracker> PLAYER_DEATH_TRACKER_CAPABILITY = CAPABILITIES.registerPlayerCapability("death_tracker", PlayerDeathTracker.class, player -> new PlayerDeathTrackerImpl(), PlayerRespawnCopyStrategy.ALWAYS);
 
     public static void touch() {
 
